20c4f73ba8b0334e8e2b87176d3119096444a6c8
Sara Gasperetti <saragasperetti@gmail.com>
2016-12-06 13:54:16 +0100

    Updated endpoint

--- src/introsde/assignment/endpoint/PeoplePublisher.java
+++ src/introsde/assignment/endpoint/PeoplePublisher.java
@@ -1,22 +1,32 @@
 package introsde.assignment.endpoint;
 import introsde.assignment.soap.PeopleImpl;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+
 import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
-	public static String SERVER_URL = "http://localhost";
-	public static String PORT = "6902";
-	public static String BASE_URL = "/soap/people";
+
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
+		String PROTOCOL = "http://";
+	    String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+	    if (HOSTNAME.equals("127.0.0.1"))
+	    {
+	        HOSTNAME = "localhost";
+	    }
+	    String PORT = "6902";
+	    String BASE_URL = "/soap/people";
 	
-	public static String getEndpointURL() {
-		return SERVER_URL+":"+PORT+BASE_URL;
-	}
- 
-	public static void main(String[] args) {
-		String endpointUrl = getEndpointURL();
-		System.out.println("Starting People Service...");
-		System.out.println("--> Published at = "+endpointUrl);
-		Endpoint.publish(endpointUrl, new PeopleImpl());
+	    if (String.valueOf(System.getenv("PORT")) != "null"){
+	        PORT=String.valueOf(System.getenv("PORT"));
+	    }
+	    
+	    String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
+	    System.out.println("Starting People Service...");
+	    System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
+	    Endpoint.publish(endpointUrl, new PeopleImpl());
     }
 }
 
